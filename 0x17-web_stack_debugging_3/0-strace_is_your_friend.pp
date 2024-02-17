1efdb2229e12180ee8804f0896fa49345a33ead9
AbdelaliLaaguidi <100788703+AbdelaliLaaguidi@users.noreply.github.com>
2024-02-17 11:06:18 +0100

    Add files via upload

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,5 @@
-# Fixes bad `phpp` extensions to `php` in the WordPress file `wp-settings.php`.
-
-exec { 'fix-wordpress':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => '/usr/local/bin/:/bin/'
+# Using strace to find out why Apache is returning a 500 error and fix it.
+exec {'replace':
+  provider => shell,
+  command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
 }
